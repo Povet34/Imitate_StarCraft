916923510d30e2eabe354b8c2d383c83d3cff571
Povet34 <70003696+Povet34@users.noreply.github.com>
2025-05-19 16:24:05 +0900

    데칼로 Selection 표현중

---
+++ Imitate_StarCraft/Assets/Scripts/Units/Worker.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+
+namespace RTS.Units
+{
+    [RequireComponent(typeof(NavMeshAgent))]
+    public class Worker : MonoBehaviour
+    {
+        [SerializeField] private Transform target;
+        private NavMeshAgent agent;
+
+        private void Awake()
+        {
+            agent = GetComponent<NavMeshAgent>();
+        }
+
+        void Update()
+        {
+            if(target != null)
+            {
+                agent.SetDestination(target.position);
+            }
+        }
+    }
+}
