973437cfb62acf81521187fd93433d99af0ccc47
Povet34 <70003696+Povet34@users.noreply.github.com>
2025-07-11 21:40:15 +0900

    Custom Nodes in Unity Behavior

---
+++ Imitate_StarCraft/Assets/Scripts/Behavior/MoveToTargetLocationAction.cs
@@ -0,0 +1,51 @@
+using System;
+using Unity.Behavior;
+using UnityEngine;
+using Action = Unity.Behavior.Action;
+using Unity.Properties;
+using UnityEngine.AI;
+
+namespace RTS.Behavior
+{
+
+    [Serializable, GeneratePropertyBag]
+    [NodeDescription(name: "Move to TargetLocation", story: "[Agent] moves to [TargetLocation] .", category: "Action", id: "6122d85655fc043b36dcef1a8d6f9556")]
+    public partial class MoveToTargetLocationAction : Action
+    {
+        [SerializeReference] public BlackboardVariable<GameObject> Agent;
+        [SerializeReference] public BlackboardVariable<Vector3> TargetLocation;
+
+        private NavMeshAgent agent;
+
+        protected override Status OnStart()
+        {
+            if (!Agent.Value.TryGetComponent(out agent))
+            {
+                Debug.LogError("Agent does not have a NavMeshAgent component.");
+                return Status.Failure;
+            }
+
+            if (Vector3.Distance(agent.transform.position, TargetLocation.Value) <= agent.stoppingDistance)
+            {
+                return Status.Success;
+            }
+
+            agent.SetDestination(TargetLocation.Value);
+            return Status.Running;
+        }
+
+        protected override Status OnUpdate()
+        {
+            if (!agent.pathPending && agent.remainingDistance <= agent.stoppingDistance)
+            {
+                return Status.Success;
+            }
+
+            return Status.Running;
+        }
+
+        protected override void OnEnd()
+        {
+        }
+    }
+}
--- Imitate_StarCraft/Assets/Scripts/Units/AbstractUnit.cs
+++ Imitate_StarCraft/Assets/Scripts/Units/AbstractUnit.cs
@@ -18,12 +18,14 @@ namespace RTS.Units
         {
             agent = GetComponent<NavMeshAgent>();
             graphAgent = GetComponent<BehaviorGraphAgent>();
+            MoveTo(transform.position);
         }
 
         protected override void Start()
         {
             base.Start();
             Bus<UnitSpawnEvent>.Raise(new UnitSpawnEvent(this));
+            MoveTo(transform.position);
         }
 
         public void MoveTo(Vector3 position)
