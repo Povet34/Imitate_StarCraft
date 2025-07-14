aa2771b9fc0ae4405305e7aaa313d24136531557
Povet34 <70003696+Povet34@users.noreply.github.com>
2025-07-14 20:11:49 +0900

    Stop 추가

--- Imitate_StarCraft/Assets/Scripts/Behavior/StopAgentAction.cs
+++ Imitate_StarCraft/Assets/Scripts/Behavior/StopAgentAction.cs
@@ -1,27 +1,25 @@
 using System;
-using Unity.Behavior;
 using UnityEngine;
 using Action = Unity.Behavior.Action;
 using Unity.Properties;
+using UnityEngine.AI;
 
-[Serializable, GeneratePropertyBag]
-[NodeDescription(name: "Stop Agent", story: "[Agent] stops moving.", category: "Action/Navigation", id: "a5151f617c06c6d5d344ad82e70d526c")]
-public partial class StopAgentAction : Action
+namespace Unity.Behavior
 {
-    [SerializeReference] public BlackboardVariable<GameObject> Agent;
-
-    protected override Status OnStart()
+    [Serializable, GeneratePropertyBag]
+    [NodeDescription(name: "Stop Agent", story: "[Agent] stops moving.", category: "Action/Navigation", id: "a5151f617c06c6d5d344ad82e70d526c")]
+    public partial class StopAgentAction : Action
     {
-        return Status.Running;
-    }
+        [SerializeReference] public BlackboardVariable<GameObject> Agent;
 
-    protected override Status OnUpdate()
-    {
-        return Status.Success;
-    }
-
-    protected override void OnEnd()
-    {
+        protected override Status OnStart()
+        {
+            if(Agent.Value.TryGetComponent(out NavMeshAgent agent))
+            {
+                agent.ResetPath();
+                return Status.Success;
+            }
+            return Status.Failure;
+        }
     }
 }
-
