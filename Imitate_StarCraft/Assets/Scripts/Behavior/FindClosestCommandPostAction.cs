751e0fbd4a57e82de41371dbf792aa6f2c395f13
Povet34 <70003696+Povet34@users.noreply.github.com>
2025-07-22 21:59:45 +0900

    Enhance FindClosestCommandPostAction logic
    Updated FindClosestCommandPostAction to search for nearby command posts using a configurable search radius and UnitSO comparison. Also made UnitSO in AbstractCommandable public for easier access, improving flexibility and integration with building detection.

--- Imitate_StarCraft/Assets/Scripts/Behavior/FindClosestCommandPostAction.cs
+++ Imitate_StarCraft/Assets/Scripts/Behavior/FindClosestCommandPostAction.cs
@@ -3,26 +3,50 @@ using Unity.Behavior;
 using UnityEngine;
 using Action = Unity.Behavior.Action;
 using Unity.Properties;
+using RTS.Units;
+using System.Collections.Generic;
 
-[Serializable, GeneratePropertyBag]
-[NodeDescription(name: "Find Closest Command Post", story: "[Unit] finds nearest [CommandPost] .", category: "Action/Units", id: "36aeca513a4121792b33b2e63efb386d")]
-public partial class FindClosestCommandPostAction : Action
+namespace RTS.Behavior
 {
-    [SerializeReference] public BlackboardVariable<GameObject> Unit;
-    [SerializeReference] public BlackboardVariable<GameObject> CommandPost;
-
-    protected override Status OnStart()
+    [Serializable, GeneratePropertyBag]
+    [NodeDescription(name: "Find Closest Command Post", story: "[Unit] finds nearest [CommandPost] .", category: "Action/Units", id: "36aeca513a4121792b33b2e63efb386d")]
+    public partial class FindClosestCommandPostAction : Action
     {
-        return Status.Running;
-    }
+        [SerializeReference] public BlackboardVariable<GameObject> Unit;
+        [SerializeReference] public BlackboardVariable<GameObject> CommandPost;
+        [SerializeReference] public BlackboardVariable<float> SearchRadius = new(10);
+        [SerializeReference] public BlackboardVariable<UnitSO> CommandPostBuilding;
 
-    protected override Status OnUpdate()
-    {
-        return Status.Success;
-    }
+        protected override Status OnStart()
+        {
+            Collider[] colliders = Physics.OverlapSphere(Unit.Value.transform.position, SearchRadius.Value, LayerMask.GetMask("Buildings"));
 
-    protected override void OnEnd()
-    {
+            List<BaseBuilding> nearbyCommandPost = new();
+            foreach (var collider in colliders)
+            {
+                if(collider.TryGetComponent(out BaseBuilding building) && building.UnitSO.Equals(CommandPostBuilding.Value))
+                {
+                    nearbyCommandPost.Add(building);
+                }
+            }
+
+            if(nearbyCommandPost.Count == 0)
+            {
+                return Status.Failure;
+            }
+
+            CommandPost.Value = nearbyCommandPost[0].gameObject;
+            return Status.Success;
+        }
+
+        protected override Status OnUpdate()
+        {
+            return Status.Success;
+        }
+
+        protected override void OnEnd()
+        {
+        }
     }
 }
 
