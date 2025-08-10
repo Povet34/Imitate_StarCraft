3124a95a330ead3b6282498bcee01f38aed084a3
Povet34 <70003696+Povet34@users.noreply.github.com>
2025-08-10 11:50:02 +0900

    Add Supplies class for player resource tracking
    Introduces the Supplies MonoBehaviour to manage and display player resources such as minerals, gas, population, and population limit using TextMeshProUGUI fields.

---
+++ Imitate_StarCraft/Assets/Scripts/Player/Supplies.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TMPro;
+using UnityEngine;
+
+namespace RTS.Player
+{
+    public class Supplies : MonoBehaviour
+    {
+        [SerializeField] TextMeshProUGUI mineralsText;
+        [SerializeField] TextMeshProUGUI gasText;
+        [SerializeField] TextMeshProUGUI popuplationText;
+
+        public static int Minerals { get; private set; }
+        public static int Gas { get; private set; }
+        public static int Population { get; private set; }
+        public static int PopulationLimit { get; private set; }
+    }
+}
