c6f2d5e3acd0c10732d3e34a6d7e8f449c2f991f
Povet34 <70003696+Povet34@users.noreply.github.com>
2025-08-20 22:17:39 +0900

    Enhance GatherCommand to handle command post interactions
    GatherCommand now supports workers returning supplies to command posts and moving to targets if not gathering. Added logic to distinguish between gatherable supplies and command posts using a serialized UnitSO reference.

--- Imitate_StarCraft/Assets/Scripts/Commands/GatherCommand.cs
+++ Imitate_StarCraft/Assets/Scripts/Commands/GatherCommand.cs
@@ -8,20 +8,34 @@ namespace RTS.Commands
     [CreateAssetMenu(fileName = "Gather Action", menuName = "AI/Commands/Gather", order = 105)]
     public class GatherCommand : ActionBase
     {
+        [SerializeField] private UnitSO commandPostSO;
         public override bool CanHandle(CommandContext context)
         {
             return context.Commandable is Worker
                 && context.Hit.collider != null
-                && context.Hit.collider.TryGetComponent(out GatherableSupply _);
+                && IsGatherableSupplyOrCommandPost(context.Hit.collider);
         }
 
         public override void Handle(CommandContext context)
         {
-            if (context.Commandable is Worker worker
-                && context.Hit.collider.TryGetComponent(out GatherableSupply supply))
+            Worker worker = context.Commandable as Worker;
+            if (context.Hit.collider.TryGetComponent(out GatherableSupply supply))
             {
                 worker.Gather(supply);
             }
+            else if (IsCommandPost(context.Hit.collider) && worker.HasSupplies)
+            {
+                worker.ReturnSupplies(context.Hit.collider.gameObject);
+            }
+            else
+            {
+                worker.MoveTo(context.Hit.collider.gameObject.transform.position);
+            }
+
         }
+
+        private bool IsGatherableSupplyOrCommandPost(Collider collider) => collider.TryGetComponent(out GatherableSupply _) || IsCommandPost(collider);
+        private bool IsCommandPost(Collider collider) => collider.TryGetComponent(out BaseBuilding building) && building.UnitSO.Equals(commandPostSO);
+
     }
 }
